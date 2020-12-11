5cfe63a22a56ac3bdaaa13071fcbee925205a2c3
chenhoward0401 <chenhoward0401@gmail.com>
2020-12-11 21:14:51 +0800

    update dependency

--- grpcPractice/calculator/calculator_client/client.go
+++ grpcPractice/calculator/calculator_client/client.go
@@ -4,60 +4,56 @@ import (
 	"context"
 	"fmt"
 	"grpcPractice/calculator/calculatorpb"
-	"grpcPractice/greet/greetpb"
 	"io"
 	"log"
-	"time"
-	"stream"
+
 	"google.golang.org/grpc"
 )
 
-
 func main() {
 	fmt.Println("Calculator Client")
-	cc,err := grpc.Dial("localhost:50052",grpc.WithInsecure())
-	if err!= nil{
-		log.Fatalf("could not connet: %v",err)
+	cc, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connet: %v", err)
 	}
 	defer cc.Close()
-	c:=calculatorpb.NewCalculatorServiceClient(cc)
+	c := calculatorpb.NewCalculatorServiceClient(cc)
 
 	// doUnary(c)
-	// doServerStreaming(c)
-	doClientStreaming(c)
+	doServerStreaming(c)
+	// doClientStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
 	fmt.Println("Starting to do a Unary RPC...")
-	req := &calculatorpb.SumRequest{ 
-		FirstNumber: 10,
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  10,
 		SecondNumber: 40,
 	}
-	res,err:= c.Sum(context.Background(),req)
-	if err!=nil{
-		log.Fatalf("error while calling Sum RPC: %v",err)
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
 	}
-	log.Printf("Response from Sum %v",res.SumRequest)
+	log.Printf("Response from Sum %v", res.SumRequest)
 }
 
-func doServerStreaming(c calculatorpb.CalculatorServiceClient){
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
 	fmt.Println("Starting to do a PrimeDecomposiotion Streaming RPC...")
 	req := &calculatorpb.PrimeNumberDecopositionRequest{
-		Number:12390392840,
+		Number: 12390392840,
 	}
-	stream,err :=c.PrimeNumberDecomposition(context.Background(),req)
-	if err!= nil {
-		log.Fatalf("error while calling PrimeDecomposition RPC: %v",err)
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling PrimeDecomposition RPC: %v", err)
 	}
 	for {
-		res,err:=stream.Recv()
-		if err== io.EOF{
+		res, err := stream.Recv()
+		if err == io.EOF {
 			break
 		}
-		if err!=nil{
-			log.Fatalf("Something happend: %v",err)
+		if err != nil {
+			log.Fatalf("Something happend: %v", err)
 		}
 		fmt.Println(res.GetPrimeFactor())
 	}
 }
-
--- grpcPractice/go.mod
+++ grpcPractice/go.mod
@@ -3,7 +3,9 @@ module grpcPractice
 go 1.14
 
 require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/golang/protobuf v1.4.2
+	go.mongodb.org/mongo-driver v1.4.4
 	google.golang.org/grpc v1.30.0
 	google.golang.org/protobuf v1.25.0
 )
