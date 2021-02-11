6c04b6e6998e75369082785336ff66421e742926
howard chen <chenhoward0401@gmail.com>
2021-02-11 11:14:11 +0800

    chore: [Howard] 1. grpcTimeout settings references 2. Server Connection Timeout 3. Client Connection Deadline

--- grpcTimeout/cmd/client/client.go
+++ grpcTimeout/cmd/client/client.go
@@ -4,17 +4,25 @@ import (
 	"context"
 	"gRPCTimeout/pb"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
 	ctx := context.Background()
+	// Set Client Deadline
+	clientDeadline := time.Now().Add(time.Duration(100 * time.Second))
+	ctx, cancel := context.WithDeadline(ctx, clientDeadline)
+	defer cancel()
+	// Set Connection
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Conn Err: %v", err)
 	}
 	defer conn.Close()
+
+	// Call TestGreet function by grpc
 	c := pb.NewHelloClient(conn)
 	res, err := c.TestGreet(ctx, &pb.GreetRequest{Say: "golang grpc000"})
 	select {
--- grpcTimeout/cmd/server/server.go
+++ grpcTimeout/cmd/server/server.go
@@ -19,34 +19,29 @@ func main() {
 	fmt.Println("hi, welcome to server..")
 	ctx := context.Background()
 	listen, _ := net.Listen("tcp", "0.0.0.0:50051")
-	grpcServer := grpc.NewServer()
 
-	clientDeadline := time.Now().Add(time.Duration(90*time.Second) * time.Millisecond)
-	ctx, cancel := context.WithDeadline(ctx, clientDeadline)
-	defer cancel()
+	opts := []grpc.ServerOption{grpc.ConnectionTimeout(120 * time.Second)}
+	grpcServer := grpc.NewServer(opts...)
 
-	// Create a cmux.
-	// m := cmux.New(listen)
-	// grpcL := m.Match(cmux.HTTP2HeaderField("content-type", "application/grpc"))
-	// s.TestGreet(ctx, *pb.GreetRequest)
-	// greeting := pb.RegisterHelloServer() //.RegisterHelloServer(s, &server{})
 	s := &Server{}
 	pb.RegisterHelloServer(grpcServer, s)
 	c := make(chan os.Signal, 1)
-	// time.Sleep(70 * time.Second)
+
 	go func() {
 		for range c {
 			grpcServer.GracefulStop()
 			<-ctx.Done()
 		}
 	}()
+
 	grpcServer.Serve(listen)
-	// go grpcServer.Serve(grpcL)
-	// m.Serve()
 }
 
+// TestGreet function for grpc
 func (s *Server) TestGreet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
-	time.Sleep(10 * time.Second)
+	fmt.Printf("begining time %v\n", time.Now())
+	time.Sleep(90 * time.Second)
+	fmt.Printf("receiving time %v\n", time.Now())
 	select {
 	default:
 	case <-ctx.Done():
